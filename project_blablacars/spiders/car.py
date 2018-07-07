e80897eccb680edcafa07a7c0f77ac5848f1b05c
BIZMIN <bismingeorge007@gmail.com>
2018-07-08 00:44:30 +0530

    adding files

--- project_blablacars/spiders/car.py
+++ project_blablacars/spiders/car.py
@@ -10,13 +10,17 @@ class CarSpider(scrapy.Spider):
     start_urls = ['https://www.blablacar.in/ride-sharing/new-delhi/chandigarh/#?fn=new+delhi&fcc=IN&tn=chandigarh&tcc=IN&sort=trip_date&order=asc&limit=10&page=1']
 
     def parse(self, response):
-	jobs = response.xpath('//div[@class="description span5"]')
+	jobs = response.xpath('//article[@class="row"]')
 	for job in jobs:
-		source=job.xpath('h3[@class="fromto"]/span[@class="from trip-roads-stop"]/text()').extract_first()
-		destination=job.xpath('h3[@class="fromto"]/span[@class="trip-roads-stop"]/text()').extract_first()
-		departure_point=job.xpath('dl[@class="geo-from"]/dd[@class="js-tip-custom"]/text()').extract_first()
-		drop_off_point=job.xpath('dl[@class="geo-to"]/dd[@class="js-tip-custom"]/text()').extract_first()
+		source=job.xpath('div[@class="description span5"]/h3[@class="fromto"]/span[@class="from trip-roads-stop"]/text()').extract_first()
+		destination=job.xpath('div[@class="description span5"]/h3[@class="fromto"]/span[@class="trip-roads-stop"]/text()').extract_first()
+		departure_point=job.xpath('div[@class="description span5"]/dl[@class="geo-from"]/dd[@class="js-tip-custom"]/text()').extract_first()
+		drop_off_point=job.xpath('div[@class="description span5"]/dl[@class="geo-to"]/dd[@class="js-tip-custom"]/text()').extract_first()
 		
-		departure_date=dateparser.parse(job.xpath('h3[@class="time u-darkGray"]/text()').extract_first())
-	
-		yield{'source':source,'destination':destination,'departure_point':departure_point,'drop_off_point':drop_off_point,'departure_date':departure_date}
+		departure_date=dateparser.parse(job.xpath('div[@class="description span5"]/h3[@class="time u-darkGray"]/text()').extract_first())
+    		name=job.xpath('div[@class="ProfileCard ProfileCard--condensed span2"]/div[@class="ProfileCard-head"]/div[@class="ProfileCard-infosBlock"]/h2[@class="ProfileCard-info ProfileCard-info--name u-truncate"]/text()').extract_first()
+		age=job.xpath('div[@class="ProfileCard ProfileCard--condensed span2"]/div[@class="ProfileCard-head"]/div[@class="ProfileCard-infosBlock"]/div[@class="ProfileCard-info"]/text()').re("([\d]+)")
+		price=job.xpath('div[@class="offer span2 u-alignRight"]/div[@class="price price-black"]/strong/span[@class=""]/text()').re("([\d]+)")
+
+
+		yield{'price':price,'age':age,'name':name,'source':source,'destination':destination,'departure_point':departure_point,'drop_off_point':drop_off_point,'departure_date':departure_date}
